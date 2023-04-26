8d8aea81869bc86dae17a2aaeaf12c5af82f3282
vaibhav <vpatil7382@gmail.com>
2023-04-26 12:59:40 +0530

    initial commit

--- regular express.js
+++ regular express.js
@@ -1,4 +1,17 @@
-const regex = /very/g
-const text = "vaibhav is a very very nice persnalyty person"
-console.log(text.replace(regex,"VERY"))
+const regex = /very/g;
+// const text = "vaibhav is a very very nice persnalyty person";
+// console.log(text.replace(regex, "VERY"));
 
+// console.log("vaibhav is the best boys in the world richest person in indea");
+
+setTimeout(function timer () {
+    console.log('you clicked the button')
+},3000)
+console.log('hiii brother')
+ 
+setTimeout(function timeout (){
+    console.log("click the button")
+},5000)
+setTimeout (function timeout () {
+    console.log("welcome to loupe")
+},7000)
